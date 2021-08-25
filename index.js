5f5fa7e118f890e717cad5746ddd274e498d04e3
xuxiaoming <787261003@qq.com>
2021-08-25 19:23:00 +0800

    完成查询水果案例

---
+++ index.js
@@ -0,0 +1,11 @@
+const fruit = prompt('请你输入查询的水果');
+switch (fruit) {
+    case '苹果':
+        alert('苹果的价格是 3.5/斤')
+        break
+    case '榴莲':
+        alert('榴莲的价格是 35/斤');
+        break
+    default:
+        alert('没有此水果 ')
+} 
